ed2197ade3dc4cb621555286976032a17893e770
Pascal Vantrepote <pvantrepote@kobo.com>
2012-08-01 08:29:33 -0400

    Fix serializer initialization with Azure

---
+++ src/impl/Serializers/NServiceBus.Serializers.Json/Config/SetJsonSerializerAsDefault.cs
@@ -0,0 +1,17 @@
+﻿using NServiceBus.ObjectBuilder;
+using NServiceBus.Config;
+using NServiceBus.Serialization;
+
+namespace NServiceBus
+{
+	class SetJsonSerializerAsDefault : INeedInitialization
+	{
+		internal static bool UseJsonSerializer;
+
+		void INeedInitialization.Init()
+		{
+			if (UseJsonSerializer && !Configure.Instance.Configurer.HasComponent<IMessageSerializer>())
+				Configure.Instance.JsonSerializer();
+		}
+	}
+}
